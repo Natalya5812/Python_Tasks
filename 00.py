24bca521db24b2ae91f1f4ed2ec4ba3bbb0579f4
«Natalya5812» <>
2022-04-15 18:37:48 +0400

    Add Tasks 0,1,2,4,5

---
+++ 00.py
@@ -0,0 +1,6 @@
+# Вывести квадрат числа
+import os
+os.system("cls")
+
+a = int (5)
+print (a**2)
---
+++ 02.py
@@ -0,0 +1,6 @@
+# 2. Найти максимальное из пяти чисел
+import os
+os.system("cls")
+
+Array = [5,10,15,40,3]
+print (max (Array))
