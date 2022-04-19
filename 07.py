4e168dda668510991d7a8b491e05b2b5f2efd18f
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-04-19 17:54:13 +0400

    correct Tasks 7

--- 07.py
+++ 07.py
@@ -1,19 +1,17 @@
-
-   
 #7. Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+
 import os
 os.system("cls")
-
 
 X = [True, False]
 Y = [True, False]
 Z = [True, False]
+print ('At: ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
 
-
-for i in range(0,2):
+for i in range (0,2):
     for j in range (0,2):
         for k in range (0,2):
             if (not(X[i] or Y[j] or Z[k])) == (not X[i] and not Y[j] and not Z[k]):
-                print (f'Выражение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - True')
+                print (f'For: X = {X[i]}, Y = {Y[j]}, Z = {Z[k]} - True')
             else:
-                print (f'Выражение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - False')
+                print ('- False')
