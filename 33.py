b9fc130d20e7a426fa5c9fc9641a81751bac9ccb
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-05-01 13:20:08 +0400

    Add Tasks 33

---
+++ 33.py
@@ -0,0 +1,42 @@
+# 33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k. 
+# *Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from itertools import *
+import os
+import random
+os.system("cls")
+
+k = random.randint (1,5)
+print (k)
+K = []
+for i in reversed(range(1, k+1)):
+    K.append (i)
+#print (K)    
+
+lst = [random.randint(0, 100) for j in range(k + 1)]
+if lst [0] == 0:
+    lst [0] = random.randint (1,100)
+print (lst)
+
+P = str('')
+for x in range (k):
+    P = P + str(lst[x]) + 'x^' + str(K[x]) + ' + '
+P = P + str(lst[-1]) + ' = 0'
+print (P)
+
+with open('33.txt','w') as data:
+    data.write (P)
+    
+     
+        
+ 
+
+# with open('33.txt','w') as data:
+#     data.write(s)
+
+
+
+
+
+
