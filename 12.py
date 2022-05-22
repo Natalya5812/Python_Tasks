92f9f627a67b8f335a0328ffbdc24365e5faa72b
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-05-22 23:46:00 +0400

    list comprehension_12

--- 12.py
+++ 12.py
@@ -1,11 +1,15 @@
-# 12. Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
+# Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
+
+
 import os
-from typing import Dict
+#from typing import Dict
 os.system("cls")
 
-#Dictionary = {}
+# #Dictionary = {}
 
 Dictionary = {n:3*n+1 for n in range (1,7)}
 print (Dictionary)
+
+
