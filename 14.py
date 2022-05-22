c9e55b84cb1b494f1b8900eb804a227a4c6f6971
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-05-22 23:47:39 +0400

    list comprehension_14

--- 14.py
+++ 14.py
@@ -1,25 +1,37 @@
-# # 14. Подсчитать сумму цифр в вещественном числе.
+# Подсчитать сумму цифр в вещественном числе.
+
 
 import os
+import random
 os.system("cls")
 
-# X = float (input ('Ведите вещественное число: '))
-# X = str(X)
-# #print (type (X))
-# X = X.replace('.','')
-# print (f'Заданное число состоит из цифр: {X}')
 
-# for i in (X):
-#     i = int (i)
-# #print (type (i))
-#     print  
-
-num = input("Введите число: ")
-sum = 0
-for i in range(0, len(num)):
-    if num[i] != ".":
-        sum += int(num[i])
-print(sum)
+a = random.uniform (1,1001)
+print (f'Сумма цифр числа {a} = {sum(map(int, str(a).replace(".", ""))) }')
 
 
 
+
+
+
+
+
+
+# # задаем случайное вещественное число из диапазона
+# a = random.uniform(1, 1001)
+# print('Задано число:', a)
+
+# a = str(a).replace('.', '')     # переводим в строковый тип, убираем точку
+# print(a)
+# summa = sum(map(int, a))        # переводим в числовой тип, считаем сумму
+# print('Сумма цифр данного числа равна:', summa, '\n')
+
+
+
+
+
+
+# #Способ 2
+# string_number = str(float(input('Enter a real number: '))).replace('.', '')
+# list_number = [int(a) for a in string_number]
+# print(f'Sum of all digits in the real number is {sum(list_number)}.')
