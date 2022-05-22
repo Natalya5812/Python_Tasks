52394013ee85ad6ca233a5f5ee5eb74195002a48
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-05-22 23:46:45 +0400

    list comprehension_11

--- 11.py
+++ 11.py
@@ -4,14 +4,34 @@
 import os
 os.system("cls")
 
-def sub (x):
-    if x in [0,1]:
-        return 1
-    else:
-        return sub (x-1)*-3
-list = []
-for N in range (1,6):
-    list.append (sub(N)) # Добавление элемента в конец списка
-print (list)
+lst =[(-3)**i for i in range (0,10)]
+print (lst)
+
+# def sub (x):
+#     if x in [0,1]:
+#         return 1
+#     else:
+#         return sub (x-1)*-3
+# list = []
+# for N in range (1,10):
+#     list.append (sub(N)) # Добавление элемента в конец списка
+# print (list)
+
+
+# def power(s):
+#     """
+#     Function will get power of number -3
+#     """
+#     new = (-3) ** s
+
+#     return new
+
+# n = 20
+# spisok=[]
+
+# for i in range(n):
+#     spisok.append(power(i))
+
+# print(f"List of elements: {spisok}")
 
 
