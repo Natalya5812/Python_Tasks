b1b472a994286a37b144c0df88a7e0a167e2897f
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-05-30 15:06:58 +0400

    PHONEBOOK

---
+++ PHONEBOOK/data_operation.py
@@ -0,0 +1,6 @@
+def add_data():
+    add_data = [input ('Фамилия: '), input ('Имя: '), input ('Контактный номер телефона: '), input ('Примечание: ')]
+    #add_data = ';'.join(add_data)
+    return (add_data)
+
+#print (add_data())
