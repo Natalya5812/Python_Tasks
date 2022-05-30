7820ea70f34e086ddfa02d313fd76718e2bc90e4
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-05-30 14:31:50 +0400

    PHONEBOOK

---
+++ controller.py
@@ -0,0 +1,29 @@
+import ui
+import data_operation
+import log
+
+
+
+
+
+
+
+
+
+
+
+def run():
+    num = ui.ui_operation()
+    while num not in range (1,4):
+        print ('Введено неверное значение, введите номер операции от 1, 2 или 3')
+    else:
+        print('Ok')
+    if num == 1:
+        print ('Введите данные')
+        log.Log_add()
+
+    if num == 2:
+        log.Log_show_row()
+
+    if num == 3:
+        log.Log_show_col() 
