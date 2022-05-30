4743e3b9354ed41e658f2de7406fc783fcd9db0a
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-05-30 15:07:48 +0400

    Delete data_operation.py

--- data_operation.py
+++
@@ -1,6 +0,0 @@
-def add_data():
-    add_data = [input ('Фамилия: '), input ('Имя: '), input ('Контактный номер телефона: '), input ('Примечание: ')]
-    #add_data = ';'.join(add_data)
-    return (add_data)
-
-#print (add_data())
